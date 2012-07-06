8b7db5039e5707f117f15cccb18a949a675d89a2
Jesse Jones <jesse9jones@gmail.com>
2012-07-06 15:26:26 -0700

    Comments

--- src/rrdf.rs
+++ src/rrdf.rs
@@ -1,15 +1,19 @@
-#[doc = "API for the rrdf library."];
+/// API for the rrdf library.
 import to_str::to_str;
 import core::dvec::*;
 import std::map::hashmap; 
 import std::time::tm;
 
+// This is a convenience for internal modules.
 import object::*;
 import query::*;
 import solution::*;
 import store::*;
 
-export subject, predicate, triple, namespace, entry, extension_fn, store, create_store, make_triple_blank, make_triple_str, make_triple_uri, get_blank_name;
+// This is the public API. Items not exported here should not be used by clients.
+// TODO: Due to resolve bugs in rustc clients will normally have to import more than just rrdf.
+export subject, predicate, triple, namespace, entry, extension_fn, store, create_store, 
+           make_triple_blank, make_triple_str, make_triple_uri, get_blank_name;
 export object;
 export solution_row, solution, selector; 
 export compile;
