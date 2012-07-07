d55d9a5cbd882e0a44fec8965fde1a1e0145cc4b
Jesse Jones <jesse9jones@gmail.com>
2012-07-06 18:38:35 -0700

    Added really ugly export code to allow clients to easily import the library.

--- src/rrdf.rs
+++ src/rrdf.rs
@@ -11,9 +11,44 @@ import solution::*;
 import store::*;
 
 // This is the public API. Items not exported here should not be used by clients.
-// TODO: Due to resolve bugs in rustc clients will normally have to import more than just rrdf.
-export subject, predicate, triple, namespace, entry, extension_fn, store, create_store, 
-           make_triple_blank, make_triple_str, make_triple_uri, get_blank_name;
-export object;
-export solution_row, solution, selector; 
-export compile;
+// TODO: Hopefully we can clean this up a lot when exporting works a bit better.
+import literal_to_object = object::literal_to_object; export literal_to_object;
+import object = object::object; export object;
+import object_methods = object::object_methods; export object_methods;
+
+import blank_value = object::blank_value; export blank_value;
+import bool_value = object::bool_value; export bool_value;
+import dateTime_value = object::dateTime_value; export dateTime_value;
+import error_value = object::error_value; export error_value;
+import float_value = object::float_value; export float_value;
+import int_value = object::int_value; export int_value;
+import invalid_value = object::invalid_value; export invalid_value;
+import iri_value = object::iri_value; export iri_value;
+import string_value = object::string_value; export string_value;
+import typed_value = object::typed_value; export typed_value;
+import unbound_value = object::unbound_value; export unbound_value;
+
+import create_store = store::create_store; export create_store;
+import entry = store::entry; export entry;
+import extension_fn = store::extension_fn; export extension_fn;
+import get_blank_name = store::get_blank_name; export get_blank_name;
+import predicate = store::predicate; export predicate;
+import make_triple_blank = store::make_triple_blank; export make_triple_blank;
+import make_triple_str = store::make_triple_str; export make_triple_str;
+import make_triple_uri = store::make_triple_uri; export make_triple_uri;
+import namespace = store::namespace; export namespace;
+import store = store::store; export store;
+import subject = store::subject; export subject;
+import triple = store::triple; export triple;
+import store_methods = store::store_methods; export store_methods;
+import to_str = store::to_str; export to_str;
+
+import compile = sparql::compile; export compile;
+import selector = sparql::selector; export selector;
+
+import solution = solution::solution; export solution;
+import solution_row = solution::solution_row; export solution_row;
+import solution_row_methods = solution::solution_row_methods; export solution_row_methods;
+import solution_methods = solution::solution_methods; export solution_methods;
+
+
